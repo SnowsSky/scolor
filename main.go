4ac03ebd713f86f520c95debc8d372980a9d1d45
SnowsSky <snowsky473@gmail.com>
2026-03-14 17:35:44 +0100

    Add: new main

--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import scolor "github.com/SnowsSky/scolor/pkg"
 
 func main() {
-	scolor.DisplayText("blue", "Welcome")
-	scolor.DisplayText("red", "to scolors\n")
+	scolor.DisplayText("blue", "Welcome ")
+	scolor.DisplayText("red", "to")
+	scolor.DisplayText("lightgreen", " scolors\n")
 
 	scolor.White.DisplayText("A simple color printing library")
 }
